55038924c9b011afcec66c0c194b99f5a6a13637
Maxoverking <avemax7@gmail.com>
2023-04-15 14:56:14 +0200

    FIRST COMMIT

--- src/main/java/com/example/btracker/Main.java
+++ src/main/java/com/example/btracker/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
     Scene scene = new Scene(fxmlLoader.load(), 660, 620);
-    stage.setTitle("Мой бюджет");
+    stage.setTitle("MY BUDGET");
     stage.setScene(scene);
     stage.show();
   }
