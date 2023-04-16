11d62951bc06efb7c1453e865ee1d9665e0d2846
Maxoverking <avemax7@gmail.com>
2023-04-16 15:38:51 +0200

    rename file

--- src/main/java/com/example/btracker/Main.java
+++ src/main/java/com/example/btracker/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+    FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("my-budget.fxml"));
     Scene scene = new Scene(fxmlLoader.load(), 700, 630);
     stage.setTitle("MY BUDGET");
     stage.setScene(scene);
