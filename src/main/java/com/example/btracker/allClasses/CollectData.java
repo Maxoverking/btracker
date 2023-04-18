6a0f45c1d2ef0df193377de36bf8beee86beeab0
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-04-18 19:17:37 +0200

    Add CollectData template

--- src/main/java/com/example/btracker/allClasses/CollectData.java
+++ src/main/java/com/example/btracker/allClasses/CollectData.java
@@ -8,17 +8,45 @@ import java.util.Map;
 public class CollectData {
 
 //    Создание класса, который будет собирать данные со всех полей и сохранять их в List.
-//            Аргументы - все поля и месяц.
-//            switch
+//    Аргументы - все поля и месяц.
+//    switch
 //    Map
 //    Создание List c названием каждого месяца.
 
+    public static Map<String, Integer> expenses = new HashMap<>();
 
-    // Map для хранения расходов по категориям и месяцам
-    Map<String, Map<String, Integer>> expenses = new HashMap<>();
+    public static void main(String[] args) {
+        // Map для хранения расходов по категориям и месяцам
+        Map<String, List<Integer>> expenses = new HashMap<>();
+        //expenses.put()
 
-    // List для хранения названий месяцев
-    ArrayList<String> months = new ArrayList<>();
+        // List для хранения названий месяцев
+//        List<String> months= new ArrayList<>();
+//        months.add("J");
+//        months.add("F");
+        List<Integer> valuesJanuary = new ArrayList<>();
+        valuesJanuary.add(20);
+        valuesJanuary.add(30);
+
+//        switch (month)
+//            case "January" ->
+
+        List<Integer> valuesFebruary = new ArrayList<>();
+        valuesFebruary.add(10);
+        valuesFebruary.add(30);
+
+        expenses.put("January", valuesJanuary);
+        expenses.put("February", valuesFebruary);
+
+        for (String i : expenses.keySet()) {
+            System.out.println("key: " + i + " value: " + expenses.get(i));
+        }
+//        for (int i = 0; i < expenses.size(); i++) {
+//
+//        }
+
+
+    }
 
 
 }
