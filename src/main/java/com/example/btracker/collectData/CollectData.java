23909772745ff9f6b8fcc235b796029e0562f69c
Dina Migunova <migunova.dina@gmail.com>
2023-04-19 19:34:16 +0200

    Change collectData - change List<Integer> to List <Map<String,Integer>>;

--- src/main/java/com/example/btracker/collectData/CollectData.java
+++ src/main/java/com/example/btracker/collectData/CollectData.java
@@ -1,30 +1,37 @@
 package com.example.btracker.collectData;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-public class CollectData {
-    /**
-     * Метод, собирающий данные по каждому месяцу в лист и возвращающий его
-     * @param income - данные по доходу за определенный месяц
-     * @param food - данные по расходу в категории Food за определенный месяц
-     * @param house - данные по расходу в категории House за определенный месяц
-     * @param shopping  - данные по расходу в категории Shopping за определенный месяц
-     * @param transport  - данные по расходу в категории Transport за определенный месяц
-     * @param entertainment  - данные по расходу в категории Entertainment за определенный месяц
-     * @param other  - данные по расходу в категории Other за определенный месяц
-     * @return - лист с данными по доходам и расходом за определенный месяц
-     */
+import java.util.Map;
 
-    public static List<Integer> collectData(int income,int food,int house,int shopping,int transport,int entertainment,int other) {
-        List<Integer> valuesAllCategory = new ArrayList<>();
-        valuesAllCategory.add(income);
-        valuesAllCategory.add(food);
-        valuesAllCategory.add(house);
-        valuesAllCategory.add(shopping);
-        valuesAllCategory.add(transport);
-        valuesAllCategory.add(entertainment);
-        valuesAllCategory.add(other);
+public class CollectData {
+
+
+    public static List<Map <String, Integer>> collectData (
+                                            int incomeValue,int foodValue,int houseValue,
+                                            int shoppingValue, int transportValue,int entertainmentValue,
+                                            int otherValue) {
+        List<Map <String, Integer>> valuesAllCategory = new ArrayList<>();
+        valuesAllCategory.add(categoryBudget(incomeValue,foodValue,houseValue, shoppingValue,
+                transportValue,entertainmentValue, otherValue));
+
 
         return valuesAllCategory;
+    }
+
+    public static Map<String, Integer> categoryBudget (  int incomeValue,int foodValue,int houseValue,
+                                                         int shoppingValue, int transportValue,int entertainmentValue,
+                                                         int otherValue) {
+        Map <String, Integer> map = new HashMap<>();
+        map.put("INCOME", incomeValue);
+        map.put("FOOD", foodValue);
+        map.put("HOUSE", houseValue);
+        map.put("SHOPPING", shoppingValue);
+        map.put("TRANSPORT", transportValue);
+        map.put("ENTERTAINMENT", entertainmentValue);
+        map.put("OTHER", otherValue);
+
+        return map;
     }
 }
