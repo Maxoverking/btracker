80c5deda1c7bf4d9ff236683cab2340beea0e38d
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-04-20 19:49:57 +0200

    Add AddMoneyTests

---
+++ src/main/java/com/example/btracker/allClasses/AddMoneyTests.java
@@ -0,0 +1,50 @@
+package com.example.btracker.allClasses;
+
+import com.example.btracker.exception.CustomException;
+import org.testng.annotations.Test;
+
+import static com.example.btracker.allClasses.Calculation.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
+
+public class AddMoneyTests {
+
+    @Test
+    public void positiveIntegers() {
+        int result = addMoney(10, 20, 100);
+        assertEquals(30, result);
+    }
+
+    @Test
+    public void PriceMinus() {
+        boolean exceptionThrown = false;
+        try {
+            addMoney(-500, 100, 200);
+        } catch (CustomException e) {
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
+    }
+
+    @Test
+    public void PriceZero() {
+        boolean exceptionThrown = false;
+        try {
+            addMoney(0, 100, 200);
+        } catch (CustomException e) {
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
+    }
+
+    @Test
+    public void PriceGreaterBudget() {
+        boolean exceptionThrown = false;
+        try {
+            addMoney(500, 100, 200);
+        } catch (CustomException e) {
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
+    }
+}
