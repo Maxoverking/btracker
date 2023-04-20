f0bfbc0ae51351ef6eb14be48f36f23013224d7a
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-04-20 19:49:21 +0200

    Add Test in CalculateBudgetTests

--- src/main/java/com/example/btracker/allClasses/CalculateBudgetTests.java
+++ src/main/java/com/example/btracker/allClasses/CalculateBudgetTests.java
@@ -1,9 +1,11 @@
 package com.example.btracker.allClasses;
 
+import com.example.btracker.exception.CustomException;
 import org.testng.annotations.Test;
 
 import static com.example.btracker.allClasses.Calculation.calculateBudget;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class CalculateBudgetTests {
 
@@ -23,5 +25,38 @@ public class CalculateBudgetTests {
     public void positiveBudgetZeroPrice() {
         int result = calculateBudget(100, 0);
         assertEquals(100, result);
+    }
+
+    @Test
+    public void negativeBudget() {
+        boolean exceptionThrown = false;
+        try {
+            calculateBudget(-500, 100);
+        } catch (CustomException e) {
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
+    }
+
+    @Test
+    public void zeroBudget() {
+        boolean exceptionThrown = false;
+        try {
+            calculateBudget(0, 100);
+        } catch (CustomException e) {
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
+    }
+
+    @Test
+    public void priceGreaterBudget() {
+        boolean exceptionThrown = false;
+        try {
+            calculateBudget(20, 100);
+        } catch (CustomException e) {
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
     }
 }
