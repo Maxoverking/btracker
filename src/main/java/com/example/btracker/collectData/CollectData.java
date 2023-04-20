eb9644d04a5851909547a5b8e1b5dabf83291c8d
Dina Migunova <migunova.dina@gmail.com>
2023-04-20 16:10:18 +0200

    fix mistakes in description CollectData();

--- src/main/java/com/example/btracker/collectData/CollectData.java
+++ src/main/java/com/example/btracker/collectData/CollectData.java
@@ -11,23 +11,24 @@ public class CollectData {
      * @param incomeValue сумма доходов
      * @param foodValue траты по категории Food
      * @param houseValue траты по категории House
-     * @param shoppingValue траты по категории траты ао категории Shopping
+     * @param shoppingValue траты по категории траты по категории Shopping
      * @param entertainmentValue траты по категории Entertainment
      * @param otherValue траты по категории Other
-     * @return возвращает лист с данными по категориям
+     * @return возвращает лист с расходами по категориям
      */
     public static List<String> collectData (
                                             int incomeValue,int foodValue,int houseValue,
                                             int shoppingValue, int transportValue,int entertainmentValue,
                                             int otherValue) {
         List<String> valuesAllCategory = new ArrayList<>();
-        valuesAllCategory.add( "INCOME"+"="+incomeValue);
-        valuesAllCategory.add("FOOD"+"="+foodValue);
-        valuesAllCategory.add("HOUSE"+"="+houseValue);
-        valuesAllCategory.add("SHOPPING"+"="+shoppingValue);
-        valuesAllCategory.add("TRANSPORT"+"="+transportValue);
-        valuesAllCategory.add("ENTERTAINMENT"+"="+entertainmentValue);
-        valuesAllCategory.add("OTHER"+"="+otherValue);
+        valuesAllCategory.add("INCOME" + "=" + incomeValue);
+        valuesAllCategory.add("FOOD" + "=" + foodValue);
+        valuesAllCategory.add("HOUSE" + "=" + houseValue);
+        valuesAllCategory.add("SHOPPING" + "=" + shoppingValue);
+        valuesAllCategory.add("TRANSPORT" + "=" + transportValue);
+        valuesAllCategory.add("ENTERTAINMENT" + "=" + entertainmentValue);
+        valuesAllCategory.add("OTHER" + "=" + otherValue);
+
         return valuesAllCategory;
     }
 }
