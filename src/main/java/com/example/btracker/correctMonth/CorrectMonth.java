a79a343928e173194f26bda9c2a0d9d0303e6303
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-04-20 13:12:54 +0200

    Add methods description, translate errors

--- src/main/java/com/example/btracker/correctMonth/CorrectMonth.java
+++ src/main/java/com/example/btracker/correctMonth/CorrectMonth.java
@@ -11,15 +11,31 @@ public class CorrectMonth {
             "january", "february", "march", "april", "may", "june",
             "july", "august", "september", "october", "november", "december");
 
+    /**
+     * Метод возвращает название месяца или выдает ошибку, если месяц введен некорректно
+     * или поле пустое.
+     *
+     * @param month месяц
+     * @return возвращает название месяца
+     */
+
     public static String correctMonth(String month) {
         if (month.isEmpty()) {
-            throw new CustomException("Нужно ввести название месяца");
+            throw new CustomException("Month name required");
         } else if (isMonthInList(month, months)) {
             return month;
         } else {
-            throw new CustomException("Введен некорректный месяц");
+            throw new CustomException("Incorrect month entered");
         }
     }
+
+    /**
+     * Метод возвращает данные, содержится ли в списке месяцев введенный месяц.
+     *
+     * @param month месяц
+     * @param months список месяцев
+     * @return возвращает данные, содержится ли в списке месяцев введенный месяц
+     */
 
     public static boolean isMonthInList(String month, List<String> months) {
         return months.contains(month);
