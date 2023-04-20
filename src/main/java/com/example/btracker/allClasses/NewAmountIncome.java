801c32ed7b99cd6c315692fa623639d3e440eab3
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-04-20 13:10:19 +0200

    Make class a record

--- src/main/java/com/example/btracker/allClasses/NewAmountIncome.java
+++ src/main/java/com/example/btracker/allClasses/NewAmountIncome.java
@@ -1,18 +1,4 @@
 package com.example.btracker.allClasses;
 
-public class NewAmountIncome {
-
-  private int income=0;
-
-  public NewAmountIncome(int income){
-    this.income = income;
-  }
-
-  public int getIncome() {
-    return income;
-  }
-
-  public void setIncome(int income) {
-    this.income = income;
-  }
+public record NewAmountIncome(int income) {
 }
