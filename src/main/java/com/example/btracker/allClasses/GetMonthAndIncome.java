4ca0da2b3ae09b250010f91d0edaec68cb5dba3c
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-04-20 13:13:14 +0200

    Make class a record

--- src/main/java/com/example/btracker/allClasses/GetMonthAndIncome.java
+++ src/main/java/com/example/btracker/allClasses/GetMonthAndIncome.java
@@ -1,19 +1,5 @@
 package com.example.btracker.allClasses;
 
-public class GetMonthAndIncome {
-  private final String month;
-  private final int income;
+public record GetMonthAndIncome(String month, int income) {
 
-  public GetMonthAndIncome(String month, int income){
-    this.month =month;
-    this.income =income;
-  }
-
-  public String getMonth() {
-    return month;
-  }
-
-  public int getIncome() {
-    return income;
-  }
 }
