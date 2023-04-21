c39ffedd5eaf353a96d54355c61cda2dafb92a3c
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-04-21 16:09:33 +0200

    Add method description

--- src/main/java/com/example/btracker/theme/Theme.java
+++ src/main/java/com/example/btracker/theme/Theme.java
@@ -8,26 +8,23 @@ import java.util.List;
 
 public class Theme {
 
-  /**
-   * Метод, меняющий цвет заливки окна приложения
-   *
-   * @param mainContainer
-   * @param theme
-   * @param outputs
-   */
+    /**
+     * Метод, меняющий цвет заливки окна приложения
+     *
+     * @param mainContainer окно приложения
+     * @param theme         тема приложения
+     * @param outputs       вывод цвета заливки
+     */
 
-  public static void theme(VBox mainContainer, CheckBox theme, List<Text> outputs) {
-    mainContainer.setStyle("-fx-background-color:black");
-    for (Text output:outputs) {
-      if (theme.isSelected()) {
-        output.setStyle("-fx-fill:white");
-      } else {
-        mainContainer.setStyle("");
-        output.setStyle("-fx-fill:black");
-      }
+    public static void theme(VBox mainContainer, CheckBox theme, List<Text> outputs) {
+        mainContainer.setStyle("-fx-background-color:black");
+        for (Text output : outputs) {
+            if (theme.isSelected()) {
+                output.setStyle("-fx-fill:white");
+            } else {
+                mainContainer.setStyle("");
+                output.setStyle("-fx-fill:black");
+            }
+        }
     }
-
-
-  }
-
 }
